0557abe34507347af97e7bcdd58a75815a3a1691
fbemb <f.bembassat@gmail.com>
2020-11-16 15:19:20 +0100

    somes fixes

--- app/models/Email.php
+++ app/models/Email.php
@@ -22,7 +22,7 @@ class Email {
             $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
             $mail->Username   = 'SetoCodeVII@gmail.com';                     // SMTP username
-            $mail->Password   = 'Test123*';                               // SMTP password
+            $mail->Password   = '******';                               // SMTP password
             $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port       = 587;                                  // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
         
